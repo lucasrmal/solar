ecaf99a59d9e335a0716320731c7f6cb61a3d7dd
Lucas Rioux-Maldague <lucasrmal@google.com>
2020-02-06 11:05:11 -0600

    Completed the API

--- api-server/api/controllers/api-controller.js
+++ api-server/api/controllers/api-controller.js
@@ -1,92 +1,131 @@
 var db = require("../models/database.js")
+require('datejs')
 
-function _query(num_datapoints, aggregation, year, month, day, hour) {
-  if (num_datapoints < 1 || num_datapoints > 1000) {
+function _query(num_datapoints, aggregation, year, month, day, hour, res) {
+  if (num_datapoints < 1 || num_datapoints > 366) {
     throw "Invalid number of data points.";
   }
 
-  var permitted = [false, false, false]; // M, D, H; year is always permitted.
-
-  if (aggregation = 'hour') {
-    permitted = [true, true, true, true];
-  } else if (aggregation = 'day') {
-    permitted = [true, true, true, false];
-  } else if (aggregation = 'month') {
-    permitted = [true, true, false, false];
-  } else if (aggregation = 'year') {
-    permitted = [true, false, false, false];
-  } else {
+  switch (aggregation) {
+    case "year":
+      month = 1;
+      // FALLTHROUGH_INTENDED
+    case "month":
+      day = 1;
+      // FALLTHROUGH_INTENDED
+    case "day":
+      hour = 0;
+      // FALLTHROUGH_INTENDED
+    case "hour":
+      break;
+    default:
     throw "Invalid aggregation.";
   }
 
-  if (!permitted[0] && month != -1) {
-    throw "Month can't be specified with this aggregation."
+  if (year < 2000 || year > 3000) {
+    throw "Invalid year.";
   }
-  if (!permitted[1] && day != -1) {
-    throw "Day can't be specified with this aggregation."
-  }
-  if (!permitted[2] && hour != -1) {
-    throw "Hour can't be specified with this aggregation."
-  }
-
-  if (month != -1 && (month < 1 || month > 12)) {
+  if (month < 1 || month > 12) {
     throw "Invalid month.";
   }
-  if (day != -1 && (day < 1 || day > 31)) {
+  if (day < 1 || day > 31) {
     throw "Invalid day.";
   }
-  if (hour != -1 && (hour < 0 || hour > 23)) {
+  if (hour < 0 || hour > 23) {
     throw "Invalid hour.";
   }
 
-  var now = new Date("December 30, 2017 21:25:10");
-  if (year == -1) {
-    year = now.getUTCFullYear();
-  }
-  if (month == -1) {
-    month = now.getUTCMonth() + 1;
-  }
-  if (day == -1) {
-    day = now.getUTCDate();
-  }
-  if (hour == -1) {
-    hour = now.getUTCHours();
-  }
-
-  var full_query = "SELECT SUM(watt_hour), ";
+  var start_date = new Date(Date.UTC(year, month-1, day, hour));
+  var end_date = new Date(start_date);
+  var aggregate_list;
 
   switch (aggregation) {
-    
+    case "year":
+      end_date.add(num_datapoints).year();
+      aggregate_list = "year";
+      break;
+    case "month":
+      end_date.add(num_datapoints).month();
+      aggregate_list = "year, month";
+      break;
+    case "day":
+      end_date.add(num_datapoints).day();
+      aggregate_list = "year, month, day";
+      break;
+    case "hour":
+      end_date.add(num_datapoints).hour();
+      aggregate_list = "year, month, day, hour";
+      break;
   }
 
+  end_date.add(-1).hour();
 
+  console.log(start_date.toISOString());
+  console.log(end_date.toISOString());
 
+  var full_query = `
+    WITH RECURSIVE dates(date) AS ( 
+      SELECT ? 
+          UNION ALL 
+      SELECT DATETIME(date, '+1 HOUR') FROM dates WHERE date<=?
+    )
+    SELECT sum(watt_hour) AS watt_hour
+    FROM (
+      SELECT strftime('%H', date) AS hour,
+             strftime('%d', date) AS day,
+             strftime('%m', date) AS month,
+             strftime('%Y', date) AS year,
+             coalesce(watt_hour, 0) AS watt_hour
+      FROM dates
+        LEFT OUTER JOIN Production p ON p.hour = strftime('%H', date) AND 
+                                        p.day = strftime('%d', date) AND 
+                                        p.month = strftime('%m', date) AND 
+                                        p.year = strftime('%Y', date)
+    )
+    GROUP BY ${aggregate_list}
+    ORDER BY ${aggregate_list};`;
 
+  // console.log(full_query);
 
-}
-
-
-exports.query = function(req, res) {
-  try {
-    query_data = _query();
-    res.json({ "status": "OK", "data": query_data });
-  } catch (err) {
-    res.json({"status": "MALFORMED_QUERY", "description" : err});
-  }
-
-
-  var query_data = [];
-  db.all(`SELECT watt_hour as watt_hour
-           FROM Production`, [], (err, rows) => {
+  db.all(full_query, [start_date.toISOString(), end_date.toISOString()], (err, rows) => {
     if (err) {
       console.error(err.message);
-      res.json({"status": "INTERNAL_SERVER_ERROR"});
+      throw "INTERNAL_SERVER_ERROR";
       return;
     }
 
+    var query_data = [];
     rows.forEach((row) => {
       query_data.push(row.watt_hour / 1000.0);
     });
     res.json({ "status": "OK", "data": query_data });
   });
+}
+
+function _parseIntParam(param, req) {
+  var integer = -1;
+  if (req.query.hasOwnProperty(param)) {
+    integer = parseInt(req.query[param], 10);
+    if (integer == NaN) { integer = 0; }
+  }
+  return integer;
+}
+
+exports.query = function(req, res) {
+  var num_datapoints = _parseIntParam("num", req);
+  var aggregation = req.query.hasOwnProperty("agg") ? req.query.agg : "";
+  var year = _parseIntParam("year", req);
+  var month = _parseIntParam("month", req);
+  var day = _parseIntParam("day", req);
+  var hour = _parseIntParam("hour", req);
+
+  try {
+    _query(num_datapoints, aggregation, year, month, day, hour, res);
+  } catch (err) {
+    if (err == "INTERNAL_SERVER_ERROR") {
+      res.json({"status": "INTERNAL_SERVER_ERROR"});
+    } else {
+      res.json({"status": "MALFORMED_QUERY", "description" : err});
+    }
+  }
 }
